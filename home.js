258e3b9abc4816ec8d67067484e8acd5e6c4379a
Brandon Schneider <brandon@webdevhq.com>
2021-03-02 08:58:50 -0700

    Nav Icon Fix, Subheading
    - Fixed an issue where mobile nav menu icons would not correctly update to the correct show or hide state.
    - Updated nav bar container feel.
    - Added a subheading.
    - Updated links.
    - Other aesthetic changes.

--- home.js
+++ home.js
@@ -1,35 +1,50 @@
-$(document).ready(()=> {
+$(document).ready(() => {
 
-    console.log("ready");
+  console.log("ready");
 
-    console.log($(this).attr("id"));
 
-  $("#dropdownMenuMobile").on("pointerdown", function() {
-    $("#hamburger-icon").toggleClass("bi bi-x");
+
+  $("#dropdownMenuMobile").on("pointerdown", function () {
+    if ($("#hamburger-icon").hasClass("bi-list")) {
+      $("#hamburger-icon").toggleClass("bi-x");
+    }
   });
 
+  $(document).on("pointerdown", function () {
+    if ($("#dropdownMenuMobile").hasClass("show") && $("#hamburger-icon").hasClass("bi-x")) {
+      console.log("foo");
+      $("#hamburger-icon").removeClass("bi-x");
+    }
+  });
+
+  /*
+  $(document).on("pointerdown", function() {
+    if ($("#hamburger-icon").hasClass("bi-x")) {
+      console.log("foo");
+      $("#hamburger-icon").removeClass("bi-x");
+    }
+  });
+*/
+
+  // Note, arrow functions do not pick up "this" and will refer to the parent container if used. Used regular func() if "this" is used.
 
 
 
-    // Note, arrow functions do not pick up "this" and will refer to the parent container if used. Used regular func() if "this" is used.
- 
-  
+  // Will need a function that jumps to the next slide on a button press, triggering the below immediately without the coroutine
+  $("#slideshow > div:gt(0)").hide();
 
-    // Will need a function that jumps to the next slide on a button press, triggering the below immediately without the coroutine
-    $("#slideshow > div:gt(0)").hide();
+  setInterval(function () {
+    $('#slideshow > div:first')
+      .hide()
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  }, 12000);
+})
 
-    setInterval(function() { 
-      $('#slideshow > div:first')
-        .hide()
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('#slideshow');
-    },  12000);
-}) 
-    
    // Need to select a parent element, could come in handy
    // $('ul li:has(ul.child)').addClass('has_child');
-  
+
 
 
